18303e7a5880b8be3bbc54272bb8057ab696402b
Xiake Sun <xiake.sun@itk-engineering.de>
2020-04-11 23:30:26 +0200

    Implement helper function for time format

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,25 @@
 #include <string>
-
+#include <iomanip>
 #include "format.h"
+#include "sstream"
 
 using std::string;
+static constexpr long ONE_HOUR = 60 * 60;
+static constexpr long ONE_MINUTE = 60;
 
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+  long hour = seconds / ONE_HOUR;
+  seconds-= hour * ONE_HOUR;
+  long min = seconds / ONE_MINUTE;
+  seconds -= min * ONE_MINUTE;
+  long sec = seconds;
+  std::ostringstream os;
+  os << std::setw(2) << std::setfill('0') << hour << ":" 
+     << std::setw(2) << std::setfill('0') << min << ":"
+     << std::setw(2) << std::setfill('0') << sec;
+  return os.str();
+}
