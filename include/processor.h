9ed2caf69f1a8ac9f0c3d5defe1c44ac303f5cce
Xiake Sun <xiake.sun@itk-engineering.de>
2020-04-11 23:29:27 +0200

    Implement function in class Processor

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,17 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
+#include <vector>
+#include <string>
 
 class Processor {
  public:
+  Processor(): prev_total_{0}, prev_idle_{0}{}
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+  long prev_total_;
+  long prev_idle_;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,14 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include "iostream"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// DONE: Return the aggregate CPU utilization
+float Processor::Utilization() {
+  long current_total = LinuxParser::Jiffies();
+  long current_idle = LinuxParser::IdleJiffies();
+  long total_diff = current_total - prev_total_;
+  long idle_diff = current_idle - prev_idle_;
+  prev_total_ = current_total;
+  prev_idle_ = current_idle;
+  return static_cast<float>(total_diff - idle_diff) / total_diff;
+}
