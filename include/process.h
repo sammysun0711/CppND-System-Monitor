fece1b5563d74e6d2de09fe01f2be4a1f6656960
Xiake Sun <xiake.sun@itk-engineering.de>
2020-04-11 23:28:58 +0200

    Implement function in class Process

--- include/process.h
+++ include/process.h
@@ -8,6 +8,8 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
+  Process(int pid, long system_uptime);
+  void UpdateStat(long ram, long uptime_);
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
@@ -18,6 +20,14 @@ class Process {
 
   // TODO: Declare any necessary private members
  private:
+ int pid_;
+ std::string user_;
+ std::string command_;
+ long ram_;
+ long active_jiffies_;
+ long system_uptime_;
+ long process_uptime_;
+ float cpu_utilization_;
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,45 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Constructor
+Process::Process(int pid, long system_uptime){
+    pid_ = pid;
+    user_ = LinuxParser::User(pid);
+    command_ = LinuxParser::Command(pid);
+    ram_ =  std::stol(LinuxParser::Ram(pid));
+    process_uptime_ = LinuxParser::UpTime(pid);
+    active_jiffies_ = LinuxParser::ActiveJiffies(pid);
+    system_uptime_ = system_uptime;
+    process_uptime_ = process_uptime_;
+    cpu_utilization_ = CpuUtilization();
+}
+// DONE: Return this process's ID
+int Process::Pid() { return pid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// DONE: Return this process's CPU utilization
+float Process::CpuUtilization() {
+    return static_cast<float>(active_jiffies_) / this->UpTime();
+}
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// DONE: Return the command that generated this process
+string Process::Command() { return command_; }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// DONE: Return this process's memory utilization
+string Process::Ram() { return std::to_string(ram_/1000); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// DONE: Return the user (name) that generated this process
+string Process::User() { return user_; }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// DONE: Return the age of this process (in seconds)
+long int Process::UpTime() { return system_uptime_ - process_uptime_; }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+// DONE: Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const& a) const { 
+    return this->cpu_utilization_ > a.cpu_utilization_;
+}
